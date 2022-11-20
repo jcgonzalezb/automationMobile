3223c27b39a4be5c6a3cc8ea856d213a0fe6456f
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-11-20 18:49:01 -0500

    Testing validations of several items on the legal list

--- src/test/java/screens/PrivacyLegalScreen.java
+++ src/test/java/screens/PrivacyLegalScreen.java
@@ -29,14 +29,76 @@ public class PrivacyLegalScreen extends BaseScreen {
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Supplemental Terms and Conditions\")")
     private AndroidElement supplementalTermsAndConditionsButton;
 
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Legal Notices\")")
+    private AndroidElement legalNoticesButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Property Rules\")")
+    private AndroidElement propertyRulesButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Electronic Communications Disclosure\")")
+    private AndroidElement electronicCommunicationsDisclosureButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Your California Privacy Rights\")")
+    private AndroidElement yourCaliforniaPrivacyRightsButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Do Not Sell My Personal Information\")")
+    private AndroidElement doNotSellMyPersonalInformationButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"In-App Maps Subject to Google Terms and Conditions\")")
+    private AndroidElement googleTermsAndConditionsButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"In-App Maps Subject to Google Privacy Policy\")")
+    private AndroidElement googlePrivacyPolicyButton;
 
     /**
      * @author Juan.Gonzalez
-     * return true if a list of legal options ment is displayed on screen, otherwise false.
+     * return true if a list of privacy & legal options is displayed on screen, otherwise false.
      */
     public boolean listOfLegalOptionsIsDisplayed() {
         return isElementAvailable(privacyAndLegalElementsList);
     }
+
+    /**
+     * @author Juan.Gonzalez
+     * return true if the Privacy Policy option is displayed on screen, otherwise false.
+     */
+    public boolean privacyPolicyOptionIsDisplayed() {
+        return isElementAvailable(privacyPolicyButton);
+    }
+
+    /**
+     * @author Juan.Gonzalez
+     * return true if the Terms of Use option is displayed on screen, otherwise false.
+     */
+    public boolean termsOfUseOptionIsDisplayed() {
+        return isElementAvailable(termsOfUseButton);
+    }
+
+    /**
+     * @author Juan.Gonzalez
+     * return true if the Supplemental Terms and Conditions option is displayed on screen, otherwise false.
+     */
+    public boolean supplementalTermsAndConditionsOptionIsDisplayed() {
+        return isElementAvailable(supplementalTermsAndConditionsButton);
+    }
+
+    /**
+     * @author Juan.Gonzalez
+     * return true if the Legal Notices option is displayed on screen, otherwise false.
+     */
+    public boolean legalNoticesOptionIsDisplayed() { return isElementAvailable(legalNoticesButton); }
+
+    /**
+     * @author Juan.Gonzalez
+     * return true if the Property Rules option is displayed on screen, otherwise false.
+     */
+    public boolean propertyRulesOptionIsDisplayed() { return isElementAvailable(propertyRulesButton); }
+    /**
+     * @author Juan.Gonzalez
+     * return true if the Electronic Communications Disclosure option is displayed on screen, otherwise false.
+     */
+    public boolean electronicCommunicationsDisclosureOptionIsDisplayed() { return isElementAvailable(electronicCommunicationsDisclosureButton); }
+
 
 
 
--- src/test/java/tests/PrivacyLegalTest.java
+++ src/test/java/tests/PrivacyLegalTest.java
@@ -25,6 +25,27 @@ public class PrivacyLegalTest extends BaseMobileTest {
         PrivacyLegalScreen privacyLegalScreen = moreOtherOptions.goToPrivacyLegalScreen();
         log.info("Validate Privacy & Legal option list");
         Assert.assertTrue(privacyLegalScreen.listOfLegalOptionsIsDisplayed(), "Privacy & Legal option list not displayed");
+        log.info("Validate Privacy Policy option");
+        Assert.assertTrue(privacyLegalScreen.privacyPolicyOptionIsDisplayed(), "Privacy Policy option not displayed");
+        log.info("Validate Terms of Use option");
+        Assert.assertTrue(privacyLegalScreen.termsOfUseOptionIsDisplayed(), "Terms of Use option not displayed");
+        log.info("Validate Supplemental Terms and Conditions option");
+        Assert.assertTrue(privacyLegalScreen.supplementalTermsAndConditionsOptionIsDisplayed(), "Supplemental Terms and Conditions option not displayed");
+        log.info("Validate Legal Notices option");
+        Assert.assertTrue(privacyLegalScreen.legalNoticesOptionIsDisplayed(), "Legal Notices option not displayed");
+        log.info("Validate Property Rules option");
+        Assert.assertTrue(privacyLegalScreen.propertyRulesOptionIsDisplayed(), "Property Rules option not displayed");
+        log.info("Validate Electronic Communications Disclosure option");
+        Assert.assertTrue(privacyLegalScreen.electronicCommunicationsDisclosureOptionIsDisplayed(), "Electronic Communications Disclosure option not displayed");
+
+
+
+
+
+
+
+
+
 
 
     }
